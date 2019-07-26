47932ef8536df5970ebce7de8598554940eaacbb
Joshua Hunt <joshhunt.info@gmail.com>
2019-07-26 16:11:36 -0400

    Animating The Ball

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 
 - Guide for the game is here:
     - https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Create_the_Canvas_and_draw_on_it
+
+- I made my first animation (and understood how I made it and how it worked!)
+
+- It's actually working out pretty nicely!
+    - Ball is bouncing off the walls properly
--- js/script.js
+++ js/script.js
@@ -1,8 +1,63 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-
+/* 
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
 ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath;
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(200, 200, 255, 0.5";
+ctx.stroke();
+ctx.closePath();
+
+*/
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+var ballRadius = 10;
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+
+  if (y + dy > canvas.height || y + dy < ballRadius) {
+    dy = -dy;
+  }
+
+  if (x + dx > canvas.width || x + dx < ballRadius) {
+    dx = -dx;
+
+  }
+  x += dx;
+  y += dy;
+}
+setInterval(draw, 10);
+
+/*
+ if(y + dy < 0) {
+    dy = -dy;
+}
+
+if(y + dy > canvas.height) {
+    dy = -dy;
+}
+Can also do with logical operators || */
