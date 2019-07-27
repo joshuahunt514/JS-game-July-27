a8204dca3443bde1d9e0fae416c8e4c4813a6452
Joshua Hunt <joshhunt.info@gmail.com>
2019-07-27 13:12:41 -0400

    Coming along

--- js/script.js
+++ js/script.js
@@ -26,6 +26,30 @@ var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
 var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = true;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = false;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = false;
+  }
+}
 
 function drawBall() {
   ctx.beginPath();
@@ -35,9 +59,18 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
 
   if (y + dy > canvas.height || y + dy < ballRadius) {
     dy = -dy;
@@ -45,11 +78,23 @@ function draw() {
 
   if (x + dx > canvas.width || x + dx < ballRadius) {
     dx = -dx;
-
   }
+
+  if (rightPressed) {
+    paddleX += 7;
+  } else if (leftPressed) {
+    paddleX -= 7;
+  }
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
+  
   x += dx;
   y += dy;
 }
+
 setInterval(draw, 10);
 
 /*
